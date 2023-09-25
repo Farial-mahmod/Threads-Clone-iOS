328f26c67cc21d396b6a77b6bafa2e6b6b639b23
Farial Mahmod <60639522+Farial-mahmod@users.noreply.github.com>
2023-09-25 16:55:25 +0600

    Updating FeedView.swift

--- Core/Feed/View/FeedView.swift
+++ Core/Feed/View/FeedView.swift
@@ -1,1 +1,33 @@
 import SwiftUI
+
+struct FeedView :  View {
+
+var body: some View {
+
+NavigationStack {
+  ScrollView(showIndicators: false){
+    // the entire block i.e. anything inside is now scrollable
+    LazyVStack {
+      ForEach(0 ... 10, id: \.self){ thread in
+        ThreadCell()
+      }
+    }
+  }.refreshable {
+    // 
+  }.navigationTitle("Threads Clone")
+  .navigationBarTitleDisplayMode(.inline)
+}.toolbar{
+  ToolbarItem(placement: .navigationBarTrailing){
+    Button {} label: { Image(systemName: "arrow.counterclockwise").foregroundColor(.black)}
+  }
+}
+}
+}
+
+struct FeedView_Previews : PreviewProvider {
+static var previews : some View {
+  NavigationStack{
+    FeedView()
+  }
+}
+}
