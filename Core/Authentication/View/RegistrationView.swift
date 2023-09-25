5bdfe3e1776118c26ab2877151293bdaf50f7d58
Farial Mahmod <60639522+Farial-mahmod@users.noreply.github.com>
2023-09-25 17:51:27 +0600

    adding RegistrationView.swift

---
+++ Core/Authentication/View/RegistrationView.swift
@@ -0,0 +1,50 @@
+import SwiftUI
+
+struct RegistrationView : View {
+
+@State private var email = ""
+@State private var password = ""
+@State private var fullname = ""
+@State private var username = ""
+  
+  var body: some View {
+    VStack {
+      
+      Image("icon")
+      .resizable()
+      .scaledToFit()
+      .padding()
+      .frame(wifth: 120, height: 120)
+
+    VStack {
+    TextField("Email: ", text: $email).modifier(ThreadsTextFieldModifier())
+    SecureField("Password: ", text: $password).modifier(ThreadsTextFieldModifier())
+    TextField("Full Name: ", text: $fullname).modifier(ThreadsTextFieldModifier())
+    TextField("User Name: ", text: $username).modifier(ThreadsTextFieldModifier())
+    }
+      Button {} label: {
+        Text("Sign Up")
+        .font(subheadline)
+        .fontWeight(.semibold)
+        .foregroundColor(.white)
+        .background(.black)
+        .cornerRadius(11)
+        .frame(width: 351, height: 45)
+      }.padding(.vertical)
+      
+      Spacer()
+      Divider()
+
+      Button {} label: {
+        HStack(spacing: 3){
+          Text("Already have an account?")
+          Text("Sign Up")
+        }
+        .foregroundColor(.black)
+        .footnote()
+      }
+      .padding(.vertical, 15)
+    }
+    
+  }
+}
