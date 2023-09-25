953137428e61ae34d56c6371275043e671160d67
Farial Mahmod <60639522+Farial-mahmod@users.noreply.github.com>
2023-09-25 19:04:16 +0600

    Continued LoginView.swift

--- Core/Authentication/View/LoginView.swift
+++ Core/Authentication/View/LoginView.swift
@@ -22,10 +22,43 @@ var body: some View {
         TextField("Enter email: ", text: $email).modifier(ThreadsTextFieldModifier())
         SecureField("Enter password: ", text: $password).modifier(ThreadsTextFieldModifier())
       }
+
+      NavigationLink{ Text("To be updated.")} label: {
+        Text("Forgot Pass?")
+        .foregroundColor(.black)
+        .frame(maxWidth: .infinity, alignment: .trailing)
+        .padding(.vertical)
+        .padding(.vertical, 28)
+        .font(.footnote)
+      }
+
+      Button {} label: {
+        Text("Login")
+        .font(subheadline)
+        .fontWeight(.semibold)
+        .foregroundColor(.white)
+        .background(.black)
+        .cornerRadius(11)
+        .frame(width: 351, height: 45)
+      }.padding(.vertical)
+      }
       
     }
   }
-  
+
+      Spacer()
+      Divider()
+
+      Button {} label: {
+        HStack(spacing: 3){
+          Text("Don\'t have an account?")
+          Text("Sign Up")
+        }
+        .foregroundColor(.black)
+        .footnote()
+      }
+      .padding(.vertical, 15)
+    }
   
 }
   
