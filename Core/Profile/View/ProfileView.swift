befdf4936e7ce24c701d886f4f41df0b1e16521f
Farial Mahmod <60639522+Farial-mahmod@users.noreply.github.com>
2023-09-26 18:02:13 +0600

    starting ProfileView.swift

---
+++ Core/Profile/View/ProfileView.swift
@@ -0,0 +1,14 @@
+import SwiftUI
+
+struct ProfileView : View {
+
+@State private var selectedFilter : ProfileThreadFilter = .threads
+@Namespace var animation
+
+private var filterBarWidth: CGFloat{
+  
+}
+
+  var body : some View {
+
+    
