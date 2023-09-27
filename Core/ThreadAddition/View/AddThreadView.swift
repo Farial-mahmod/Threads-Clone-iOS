05574ce4b4b71ab22ea03a6aa8ec80b83c3a7de7
Farial Mahmod <60639522+Farial-mahmod@users.noreply.github.com>
2023-09-27 16:05:11 +0600

    Updating AddThreadView.swift

--- Core/ThreadAddition/View/AddThreadView.swift
+++ Core/ThreadAddition/View/AddThreadView.swift
@@ -11,9 +11,54 @@ struct AddThreadView : View {
            CircularProfileImageView()
 
            VStack(alignment: .leading, spacing: 5){
-             Text("Kemon achen?").fontWeight(.semibold)
+             Text("Threads").fontWeight(.semibold)
              TextField("Write a thread ..", text: $caption, axis: .vertical).font(.footnote)
            }
+           
            Spacer()
+
+           if !caption.isEmpty {
+             Button {
+                     caption = ""
+                    } label: {
+               Image(systemName: "xmark")
+               .resizable()
+               .frame(width: 12, height: 12)
+               .foregroundColor(.gray)
+             }
+           }
+         }
+         Spacer()
+       }
+       .padding()
+       .navigationTitle("New Thread")
+       .navigationBarTitleDisplayMode(.inline)
+       .toolbar {
+
+        ToolbarItem(placement: .navigationBarLeading){
+          Button{
+            dismiss()
+          }.font(.subheadline)
+          .foregroundColor(.black)
+       }
+
+        ToolbarItem(placement: .navigationBarTrailing){
+          Button{
+            
+          }.font(.subheadline)
+          .foregroundColor(.black)
+          .disabled(caption.isEmpty)
+          .opacity(caption.isEmpty ? 0.1 : 1)
+       }
+       }
+     }
+   }
+}
+
+struct AddThreadView_Previews : View {
+  static var previews : View {
+    AddThreadView()
+  }
+}
            
   
