56c02db9d0a2d33058b1a824b9ab0c34cc495137
Farial Mahmod <60639522+Farial-mahmod@users.noreply.github.com>
2023-09-27 19:26:24 +0600

    starting RegistrationViewModel.swift

---
+++ Core/Authentication/ViewModel/RegistrationViewModel.swift
@@ -0,0 +1,19 @@
+import Foundation
+
+class ViewModel : ObservableObject {
+  @Published var email = ""
+  @Published var password = ""
+  @Published var fullname = ""
+  @Published var username = ""
+
+  @MainActor
+  func createUser() async throws {
+    try await AuthService.shared.createUser(
+      withEmail: email,
+      password: password,
+      fullname: fullname,
+      username: username
+    )
+  }
+  
+}
