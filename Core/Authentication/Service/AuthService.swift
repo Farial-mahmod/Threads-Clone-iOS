b2503d42135660881ce4144f1b71583ddb5664e7
Farial Mahmod <60639522+Farial-mahmod@users.noreply.github.com>
2023-09-27 19:45:35 +0600

    Update and rename Core/Authentication/View/Service/AuthService.swift to Core/Authentication/Service/AuthService.swift

--- Core/Authentication/Service/AuthService.swift
+++ Core/Authentication/Service/AuthService.swift
@@ -2,15 +2,20 @@ import Firebase
 
 class AuthService {
 
+  @Published var userSession : FirebaseAuth.User?
   static let shared = AuthService()
 
   @MainActor
-  func login(withEmail email: String, password: String, fullname: String, username: String) throws async {
+  func login(withEmail email: String, password: String, fullname: String, username: String) async throws {
     do {
       let result = try await Auth.auth().createUser(withEmail: email, password: password)
       print("User generated: \(result.user.uid)")
     } catch {
       print(error.localizedDescription)
     }
+  }
+  func signOut(){
+    try? Auth.auth().signOut()
+    self.userSession = nil
   }
 }
