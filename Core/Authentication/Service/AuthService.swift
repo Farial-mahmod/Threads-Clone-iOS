775216792952eb66cd5e73b2714f58b4d2f36a73
Farial Mahmod <60639522+Farial-mahmod@users.noreply.github.com>
2023-09-28 02:29:30 +0600

    Updating AuthService.swift

--- Core/Authentication/Service/AuthService.swift
+++ Core/Authentication/Service/AuthService.swift
@@ -10,14 +10,27 @@ class AuthService {
   }
 
   @MainActor
-  func login(withEmail email: String, password: String, fullname: String, username: String) async throws {
+  func login(withEmail email: String, password: String) async throws {
     do {
-      let result = try await Auth.auth().createUser(withEmail: email, password: password)
+      let result = try await Auth.auth().signIn(withEmail: email, password: password)
+      self.userSession = result.user
       print("User generated: \(result.user.uid)")
     } catch {
       print(error.localizedDescription)
     }
   }
+
+    @MainActor
+  func createUser(withEmail email: String, password: String, fullname: String, username: String) async throws {
+    do {
+      let result = try await Auth.auth().createUser(withEmail: email, password: password)
+      self.userSession = result.user
+      print("User generated: \(result.user.uid)")
+    } catch {
+      print(error.localizedDescription)
+    }
+  }
+  
   func signOut(){
     try? Auth.auth().signOut()
     self.userSession = nil
